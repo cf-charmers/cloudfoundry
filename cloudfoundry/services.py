4fd31ad1e38c2834258e89aa655c89e505a6839d
Whit Morriss <whit.morriss@canonical.com>
2014-07-14 16:07:55 -0500

    flesh out cc jobs & services w/ context todos

--- cloudfoundry/services.py
+++ cloudfoundry/services.py
@@ -4,29 +4,103 @@ __all__ = ['SERVICES']
 
 SERVICES = {
     'cc-clock-v1': {
-        'summary': 'Cloud Controller Clock',
+        'summary': "A shared clock",
         'description': '',
-        'jobs': [{
-            'job_name': 'cloud-controller-clock',
-            'mapping': (),
-            "provided_data": [contexts.CloudControllerRelation],
-            'required_data': [contexts.NatsRelation,
-                              contexts.MysqlRelation,
-                              ],
-        }]
+        'jobs':[{
+            'job_name': 'cloud_controller_clock',
+            'mapping': {
+                'nats.(\w+)': r'properties.nats.\1', #TODO: use callback for list
+                'db.(\w+)': r'properties.ccdb.\1',
+                'uaa.(\w+)': r'properties.uaa.\1',
+                'cc.(\w+)': r'properties.cc.\1',
+                'login.(\w+)': r'properties.login.\1',
+                'syslog_aggregator.(\w+)': r'properties.syslog_aggregator.\1',
+                #TODO see: logger_endpoint.. may need to extend loggregator context
+                'loggregator_endpoint.(\w+)':r'properties.loggregator_endpoint.\1'
+                },
+            'provided_data':[], #TODO: context.ClockRelation
+            'required_data':[contexts.NatsRelation,
+                             contexts.LoggregatorRelation,
+                             contexts.MysqlRelation,
+                             contexts.CloudControllerRelation,
+                             #TODO: context.LoginRelation
+                             #TODO: context.UAARelation,
+                             #TODO: context.SyslogAggregatorRelation
+                             # diego is coming
+                ]
+            }],
+
     },
-    'cloud-controller-v1': {
+
+    'cc-v1': {
         'summary': 'CF Cloud Controller, the brains of the operation',
         'description': '',
         'jobs': [{
+            'job_name': 'cf_cloudcontroller_ng',
+            'mapping': {
+                'cc.(\w+)': r'properties.cc.\1',
+                'nats.(\w+)': r'properties.nats.\1', #TODO: use callback for list
+                'uaa.(\w+)': r'properties.uaa.\1',
+                'db.(\w+)': r'properties.ccdb.\1',
+                'dea.(\w+)': r'properties.dea_next.\1',
+                'login.(\w+)': r'properties.login.\1',
+                'syslog_aggregator.(\w+)': r'properties.syslog_aggregator.\1',
+                #TODO see: logger_endpoint.. may need to extend loggregator context
+                'loggregator_endpoint.(\w+)':r'properties.loggregator_endpoint.\1'
+            },
+            "provided_data": [contexts.CloudControllerRelation],
             'job_name': 'cf-cloudcontroller-ng',
             'mapping': (),
             'provided_data': [contexts.CloudControllerRelation],
             'required_data': [contexts.NatsRelation,
                               contexts.RouterRelation,
                               contexts.MysqlRelation,
+                              contexts.LoggregatorRelation,
+                              #TODO: context.ClockRelation,
+                              #TODO: context.UAARelation,
+                              #TODO: context.DEARelation
+                              #TODO: context.LoginRelation
+                              #TODO: context.SyslogAggregatorRelation
+                              # diego is coming
+                              # contexts.BundleConfig,
+                              # All job context keys
+                              # get processed by a name mapper
                               ],
         }]
+    },
+
+    'cc_worker-v1':{
+        'summary': "Worker for cc",
+        'description': '',
+        'jobs': [
+            {'job_name': 'cloud_controller_worker',
+             'mapping': {
+                'nats.(\w+)': r'properties.nats.\1', #TODO: use callback for list
+                'uaa.(\w+)': r'properties.uaa.\1',
+                'db.(\w+)': r'properties.ccdb.\1',
+                'dea.(\w+)': r'properties.dea_next.\1',
+                'login.(\w+)': r'properties.login.\1',
+                'syslog_aggregator.(\w+)': r'properties.syslog_aggregator.\1',
+                #TODO see: logger_endpoint.. may need to extend loggregator context
+                'loggregator_endpoint.(\w+)':r'properties.loggregator_endpoint.\1'
+            },
+            "provided_data": [],
+            'required_data': [contexts.NatsRelation,
+                              contexts.RouterRelation,
+                              contexts.MysqlRelation,
+                              contexts.LoggregatorRelation,
+                              #TODO: context.UAARelation,
+                              #TODO: context.DEARelation
+                              #TODO: context.LoginRelation
+                              #TODO: context.SyslogAggregatorRelation
+                              # diego is coming
+                              # contexts.BundleConfig,
+                              # All job context keys
+                              # get processed by a name mapper
+                              ],
+             }
+            ]
+
     },
 
     'dea-v1': {
@@ -69,6 +143,8 @@ SERVICES = {
         }],
     },
 
+    'nats_stream_forwarder-v1':{},
+
     'router-v1': {
         'service': 'router',
         'summary': 'CF Router',
@@ -84,4 +160,32 @@ SERVICES = {
         }],
 
     },
+
+    'uaa-v1': {
+        'service': 'uaa',
+        'summary': 'CF Oauth2 for identity management service',
+        'jobs':[
+            {'job_name': 'uaa',
+             'ports': [8080],
+             'mapping':{
+                 'uaa.(\w+)': r'properties.uaa.\1',
+                 'db.(\w+)': r'properties.uaa.db.\1'
+                },
+             'provided_data':[],
+             'required_data':[contexts.MysqlRelation]
+            }
+        ]
+    },
+
+    'login-v1': {},
+
+    'loggregator-v1': {},
+
+    'loggregator_trafficcontroller-v1': {},
+
+    'hm9000-v1': {},
+
+    'syslog_aggregator-v1': {},
+
+    'haproxy-v1': {}
 }
