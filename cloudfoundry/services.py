b3ae15bbe8f0bb39004d8838d308c886badc8638
Benjamin Saller <benjamin.saller@canonical.com>
2014-07-14 12:32:51 -0700

    sketch out some service blocks

--- cloudfoundry/services.py
+++ cloudfoundry/services.py
@@ -3,24 +3,59 @@ import contexts
 __all__ = ['SERVICES']
 
 SERVICES = {
-    'cc-clock-v1': {},
+    'cc-clock-v1': {
+        'summary': 'Cloud Controller Clock',
+        'description': '',
+        'jobs': [{
+            'job_name': 'cloud-controller-clock',
+            'mapping': (),
+            "provided_data": [contexts.CloudControllerRelation],
+            'required_data': [contexts.NatsRelation,
+                              contexts.MysqlRelation,
+                              ],
+        }]
+    },
     'cloud-controller-v1': {
         'summary': 'CF Cloud Controller, the brains of the operation',
         'description': '',
         'jobs': [{
             'job_name': 'cf-cloudcontroller-ng',
-            'mapping': {
-                # process the final context before feeding it to erb render
-                # using this rel_key to property path mapping
-            },
-            "provided_data": [contexts.CloudControllerRelation],
+            'mapping': (),
+            'provided_data': [contexts.CloudControllerRelation],
             'required_data': [contexts.NatsRelation,
                               contexts.RouterRelation,
                               contexts.MysqlRelation,
-                              # contexts.BundleConfig,
-                              # All job context keys
-                              # get processed by a name mapper
                               ],
+        }]
+    },
+
+    'dea-v1': {
+        'summary': 'DEA runs CF apps in containers',
+        'description': '',
+        'jobs': [{
+            'job_name': 'dea_next',
+            'mapping': (
+                ('dea.(\w+)', r'properties.dea_next.\1'),
+            ),
+            'required_data': [
+                contexts.NatsRelation,
+                contexts.LogRouterRelation,
+                contexts.RouterRelation
+            ],
+        }]
+
+    },
+
+    'dea-logging-agent-v1': {
+        'summary': 'Logging Agent for DEA',
+        'description': '',
+        'jobs': [{
+            'job_name': 'dea_logging_agent',
+            'mapping': (('dea_logging_agent.(\w+)', r'properties'),),
+            'required_data': [
+                contexts.NatsRelation,
+                contexts.RouterRelation
+            ]
         }]
     },
 
@@ -29,9 +64,7 @@ SERVICES = {
         'summary': 'NATS message bus for CF',
         'jobs': [{
             'job_name': 'nats',
-            'mapping': {
-                'nats.(\w+)': r'properties.nats.\1'
-            },
+            'mapping': (('nats.(\w+)', r'properties.nats.\1'),),
             'provided_data': [contexts.NatsRelation],
         }],
     },
@@ -42,9 +75,7 @@ SERVICES = {
         'jobs': [{
             'job_name': 'gorouter',
             'ports': [80],
-            'mapping': {
-                'router.(\w+)': r'properties.router.\1'
-            },
+            'mapping': (('router.(\w+)', r'properties.router.\1'),),
             'provided_data': [contexts.RouterRelation],
             'required_data': [contexts.NatsRelation,
                               contexts.LogRouterRelation],
