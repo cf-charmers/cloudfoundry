59898171377b77e89473830cc705062d0ba3382a
Whit Morriss <whit.morriss@canonical.com>
2014-07-14 16:18:41 -0500

    stub more services (commented out)

--- cloudfoundry/releases.py
+++ cloudfoundry/releases.py
@@ -5,13 +5,20 @@
 # mapping
 COMMON_SERVICES = [
     ('cloud-controller-v1', 'cc'),
+    #('cc-clock-v1', 'cc-clock'),
+    #('cc-worker-v1', 'cc-worker'),
     ('router-v1', 'router'),
     ('nats-v1', 'nats'),
+    ('uaa-v1', 'uaa'),
     #('dea-v1', 'dea'),
-    #('uaa-v1', 'uaa'),
-    #('logrouter-v1', 'logrouter'),
+    #('login-v1', 'login')
+    #('dea-logging-agent-v1', 'dea-logging'),
+    #('nats-stream-forwarder', 'nats-sf'),
     #('loggregator-v1', 'loggregator'),
+    #('loggregator-trafficcontrol-v1', 'loggregator-trafficcontrol'),
     #('hm9000-v1', 'hm'),
+    #('syslog-aggregator-v1', 'syslog-aggregator')
+    #('haproxy-v1', 'haproxy')
 
     ('cs:trusty/mysql', 'mysql'),
     ('cs:~hazmat/trusty/etcd', 'etcd'),
@@ -21,6 +28,7 @@ COMMON_SERVICES = [
 # to become deployment relations for a given topo.
 COMMON_RELATIONS = [
     ('nats:nats', 'router:nats'),
+    ('uaa:db', 'mysql:db')
 ]
 
 COMMON_UPGRADES = []
