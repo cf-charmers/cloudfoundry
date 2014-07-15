42c6509fbe9156807b2c2ce699dc45d30b8b3eae
Cory Johns <cory.johns@canonical.com>
2014-07-15 14:41:54 -0400

    flake8 cleanup in cf/servies.py

--- cloudfoundry/services.py
+++ cloudfoundry/services.py
@@ -6,19 +6,19 @@ SERVICES = {
     'cloud-controller-clock-v1': {
         'summary': "A shared clock",
         'description': '',
-        'jobs':[{
+        'jobs': [{
             'job_name': 'cloud_controller_clock',
             'mapping': {
-                'nats.(\w+)': r'properties.nats.\1', #TODO: use callback for list
+                'nats.(\w+)': r'properties.nats.\1',  # TODO: use callback for list
                 'db.(\w+)': r'properties.ccdb.\1',
                 'uaa.(\w+)': r'properties.uaa.\1',
                 'cc.(\w+)': r'properties.cc.\1',
                 'login.(\w+)': r'properties.login.\1',
                 'syslog_aggregator.(\w+)': r'properties.syslog_aggregator.\1',
-                #TODO see: logger_endpoint.. may need to extend loggregator context
-                'loggregator_endpoint.(\w+)':r'properties.loggregator_endpoint.\1'
+                # TODO see: logger_endpoint.. may need to extend loggregator context
+                'loggregator_endpoint.(\w+)': r'properties.loggregator_endpoint.\1'
                 },
-            'provided_data':[], #TODO: context.ClockRelation
+            'provided_data': [],  # TODO: context.ClockRelation
             'required_data':[contexts.NatsRelation,
                              contexts.LoggregatorRelation,
                              contexts.MysqlRelation,
@@ -27,7 +27,7 @@ SERVICES = {
                              #TODO: context.UAARelation,
                              #TODO: context.SyslogAggregatorRelation
                              # diego is coming
-                ]
+                             ]
             }],
 
     },
@@ -39,14 +39,14 @@ SERVICES = {
             'job_name': 'cf_cloudcontroller_ng',
             'mapping': {
                 'cc.(\w+)': r'properties.cc.\1',
-                'nats.(\w+)': r'properties.nats.\1', #TODO: use callback for list
+                'nats.(\w+)': r'properties.nats.\1',  # TODO: use callback for list
                 'uaa.(\w+)': r'properties.uaa.\1',
                 'db.(\w+)': r'properties.ccdb.\1',
                 'dea.(\w+)': r'properties.dea_next.\1',
                 'login.(\w+)': r'properties.login.\1',
                 'syslog_aggregator.(\w+)': r'properties.syslog_aggregator.\1',
                 #TODO see: logger_endpoint.. may need to extend loggregator context
-                'loggregator_endpoint.(\w+)':r'properties.loggregator_endpoint.\1'
+                'loggregator_endpoint.(\w+)': r'properties.loggregator_endpoint.\1'
             },
             'provided_data': [contexts.CloudControllerRelation],
             'required_data': [contexts.NatsRelation,
@@ -66,35 +66,35 @@ SERVICES = {
         }]
     },
 
-    'cloud-controller-worker-v1':{
+    'cloud-controller-worker-v1': {
         'summary': "Worker for cc",
         'description': '',
         'jobs': [
             {'job_name': 'cloud_controller_worker',
              'mapping': {
-                'nats.(\w+)': r'properties.nats.\1', #TODO: use callback for list
-                'uaa.(\w+)': r'properties.uaa.\1',
-                'db.(\w+)': r'properties.ccdb.\1',
-                'dea.(\w+)': r'properties.dea_next.\1',
-                'login.(\w+)': r'properties.login.\1',
-                'syslog_aggregator.(\w+)': r'properties.syslog_aggregator.\1',
-                #TODO see: logger_endpoint.. may need to extend loggregator context
-                'loggregator_endpoint.(\w+)':r'properties.loggregator_endpoint.\1'
-            },
-            "provided_data": [],
-            'required_data': [contexts.NatsRelation,
-                              contexts.RouterRelation,
-                              contexts.MysqlRelation,
-                              contexts.LoggregatorRelation,
-                              #TODO: context.UAARelation,
-                              #TODO: context.DEARelation
-                              #TODO: context.LoginRelation
-                              #TODO: context.SyslogAggregatorRelation
-                              # diego is coming
-                              # contexts.BundleConfig,
-                              # All job context keys
-                              # get processed by a name mapper
-                              ],
+                 'nats.(\w+)': r'properties.nats.\1',  # TODO: use callback for list
+                 'uaa.(\w+)': r'properties.uaa.\1',
+                 'db.(\w+)': r'properties.ccdb.\1',
+                 'dea.(\w+)': r'properties.dea_next.\1',
+                 'login.(\w+)': r'properties.login.\1',
+                 'syslog_aggregator.(\w+)': r'properties.syslog_aggregator.\1',
+                 #TODO see: logger_endpoint.. may need to extend loggregator context
+                 'loggregator_endpoint.(\w+)': r'properties.loggregator_endpoint.\1'
+             },
+             'provided_data': [],
+             'required_data': [contexts.NatsRelation,
+                               contexts.RouterRelation,
+                               contexts.MysqlRelation,
+                               contexts.LoggregatorRelation,
+                               #TODO: context.UAARelation,
+                               #TODO: context.DEARelation
+                               #TODO: context.LoginRelation
+                               #TODO: context.SyslogAggregatorRelation
+                               # diego is coming
+                               # contexts.BundleConfig,
+                               # All job context keys
+                               # get processed by a name mapper
+                               ],
              }
             ]
 
@@ -141,7 +141,7 @@ SERVICES = {
         }],
     },
 
-    'nats-stream-forwarder-v1':{},
+    'nats-stream-forwarder-v1': {},
 
     'router-v1': {
         'service': 'router',
@@ -163,16 +163,16 @@ SERVICES = {
     'uaa-v1': {
         'service': 'uaa',
         'summary': 'CF Oauth2 for identity management service',
-        'jobs':[
+        'jobs': [
             {'job_name': 'uaa',
              'ports': [8080],
              'mapping':{
                  'uaa.(\w+)': r'properties.uaa.\1',
                  'db.(\w+)': r'properties.uaa.db.\1'
-                },
-             'provided_data':[],
+                 },
+             'provided_data': [],
              'required_data':[contexts.MysqlRelation]
-            }
+             }
         ]
     },
 
