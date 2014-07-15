93099710809ec8fb578cef054a035a7814dca154
Whit Morriss <whit.morriss@canonical.com>
2014-07-15 11:55:46 -0500

    get worker charmid right, add commas

--- cloudfoundry/releases.py
+++ cloudfoundry/releases.py
@@ -5,19 +5,19 @@
 # mapping
 COMMON_SERVICES = [
     ('cloud-controller-v1', 'cc'),
-    #('cc-clock-v1', 'cc-clock'),
-    #('cc-worker-v1', 'cc-worker'),
+    ('cloud-controller-clock-v1', 'cc-clock'),
+    ('cloud-controller-worker-v1', 'cc-worker'),
     ('router-v1', 'router'),
     ('nats-v1', 'nats'),
     ('uaa-v1', 'uaa'),
     #('dea-v1', 'dea'),
-    #('login-v1', 'login')
+    #('login-v1', 'login'),
     #('dea-logging-agent-v1', 'dea-logging'),
     #('nats-stream-forwarder', 'nats-sf'),
     #('loggregator-v1', 'loggregator'),
     #('loggregator-trafficcontrol-v1', 'loggregator-trafficcontrol'),
     #('hm9000-v1', 'hm'),
-    #('syslog-aggregator-v1', 'syslog-aggregator')
+    #('syslog-aggregator-v1', 'syslog-aggregator'),
     #('haproxy-v1', 'haproxy')
 
     ('cs:trusty/mysql', 'mysql'),
